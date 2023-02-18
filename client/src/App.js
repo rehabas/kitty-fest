a0de72664988f3b5fe337c48983a4b39f1bbdd72
rehabas <reremo45@gmail.com>
2023-02-19 00:59:54 +0200

    design register page relates #5

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Login from './pages/Login';
+import Register from './pages/Register';
 
 // import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Register />
     </div>
   );
 }
